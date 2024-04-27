a8e26cb21eb9c8983ec20c6fbad8a65a90359b7e
Æscanor <113015001+Aescanor@users.noreply.github.com>
2024-04-27 11:31:23 +0200

    composants courants - ongles => ok

--- 6.Créer-des-composants-courants/6.Créer-des-composants-courants/src/App.jsx
+++ 6.Créer-des-composants-courants/6.Créer-des-composants-courants/src/App.jsx
@@ -1,9 +1,11 @@
-import ModalButton from "./lessons/1.Créer-une-fenêtre-modale-avec-un-portal/ModalButton";
+// import ModalButton from "./lessons/1.Créer-une-fenêtre-modale-avec-un-portal/ModalButton";
+import Tabs from "./lessons/2.Coder-des-onglets/Tabs";
+
 
 function App() {
   return (
     <div className="min-h-screen bg-slate-700 pt-10">
-      <ModalButton />
+      <Tabs />
     </div>
   );
 }
